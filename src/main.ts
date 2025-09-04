d2f832c2e778d9f241d2f4b3d0b3a6943a8405fb
王云海 <adoin@qq.com>
2025-09-04 14:35:45 +0800

    rm: unused code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
 import 'uno.css'
 import '@unocss/reset/tailwind.css'
 import './style.css'
@@ -10,5 +9,4 @@ const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
-app.use(router)
 app.mount('#app')
